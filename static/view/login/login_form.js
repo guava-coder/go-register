1315b28e3ff92bdf92a8e5ee35ce488c9036c33c
Wang-Kai-Chi <ericwangcatch@gmail.com>
2024-01-23 12:34:07 +0800

    add new link to static/index.html, add show password feature to static/view/login/login_form.html

--- static/view/login/login_form.js
+++ static/view/login/login_form.js
@@ -1,12 +1,33 @@
 import JwtController from '../../js/controller/jwt_controller.js'
 
-const form = document.querySelector('#loginForm')
-
-form.addEventListener('submit', function (e) {
+document.querySelector('#loginForm').addEventListener('submit', function (e) {
   e.preventDefault()
   const formData = new FormData(e.target)
   const userData = Object.fromEntries(formData)
 
   JwtController().login(JSON.stringify(userData))
-    .then(data => { if (data !== undefined)location.reload() })
+    .then(data => { if (data !== undefined) location.reload() })
 })
+
+const showPsw = document.querySelector('#showPsw')
+showPsw.onclick = () => {
+  const password = document.querySelector('#password')
+  const classes = showPsw.classList
+  const ishow = 'bi-eye-fill'
+  const ihide = 'bi-eye-slash-fill'
+  const showPassword = () => {
+    classes.remove(ihide)
+    classes.add(ishow)
+    password.type = 'text'
+  }
+  const hidePassword = () => {
+    classes.remove(ishow)
+    classes.add(ihide)
+    password.type = 'password'
+  }
+  if (classes.contains(ishow)) {
+    hidePassword()
+  } else if (classes.contains(ihide)) {
+    showPassword()
+  }
+}
