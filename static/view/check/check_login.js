887d5a32d843669669330b2ada658114c65e8ed7
Wang-Kai-Chi <ericwangcatch@gmail.com>
2024-02-26 16:00:31 +0800

    update static/view/main.html

--- static/view/check/check_login.js
+++ static/view/check/check_login.js
@@ -1,25 +1,29 @@
 import UserController from '../../js/controller/user_controller.js'
 
-const fetchHTMLPage = (url = '', handleData = () => {}) => {
-  const content = document.querySelector('#content')
-  fetch(url)
-    .then(res => { return res.text() })
+CheckLogin()
+
+function CheckLogin () {
+  const fetchHTMLPage = (url = '', handleData = () => {}) => {
+    const content = document.querySelector('#content')
+    fetch(url)
+      .then(res => { return res.text() })
+      .catch(err => console.log(err))
+      .then(data => {
+        content.innerHTML = data
+        htmx.process(content)
+        handleData()
+      })
+  }
+
+  UserController().findUserData()
     .catch(err => console.log(err))
-    .then(data => {
-      content.innerHTML = data
-      htmx.process(content)
-      handleData()
+    .then(user => {
+      if (user === undefined) {
+        fetchHTMLPage('/static/view/check/default_page.html')
+      } else {
+        fetchHTMLPage('/static/view/check/login_success.html', () => {
+          document.querySelector('#welcome').innerHTML = `<h2>Welcome ${user.User.Name}!</h2>`
+        })
+      }
     })
 }
-
-UserController().findUserData()
-  .catch(err => console.log(err))
-  .then(user => {
-    if (user === undefined) {
-      fetchHTMLPage('/static/view/check/default_page.html')
-    } else {
-      fetchHTMLPage('/static/view/check/login_success.html', () => {
-        document.querySelector('#welcome').innerHTML = `<h2>Welcome ${user.User.Name}!</h2>`
-      })
-    }
-  })
