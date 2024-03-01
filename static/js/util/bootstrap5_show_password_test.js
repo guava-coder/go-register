9960260b27c3c069f622359b2a708dee2e41bff0
Wang-Kai-Chi <ericwangcatch@gmail.com>
2024-03-01 15:25:24 +0800

    add static/test/widget/

--- static/js/util/bootstrap5_show_password_test.js
+++
@@ -1,4 +0,0 @@
-import Bootstrap5ShowPassword from './bootstrap5_show_password.js'
-
-const showPsw = document.querySelector('#showPsw')
-showPsw.onclick = () => Bootstrap5ShowPassword(showPsw, '#password')
--- static/view/register/register_form.js
+++ static/view/register/register_form.js
@@ -1,6 +1,5 @@
 import UserController from '../../js/controller/user_controller.js'
-import Bootstrap5ShowPassword from '../../js/util/bootstrap5_show_password.js'
-import Bootstrap5Validation from '../../js/util/bootstrap5_vaildation.js'
+import Bootstrap5Validation from '../../widget/inputs/bootstrap5_validation.js'
 import EmailController from '../../js/controller/email_controller.js'
 import GotoVerifyPage from '../verification/go_to_verify_page.js'
 
@@ -27,30 +26,16 @@ function RegisterForm () {
       })
   }
 
-  const va = Bootstrap5Validation()
-
-  const psw = document.querySelector('#password')
-  va.passwordValidate(psw)
-
-  const email = document.querySelector('#email')
-  va.emailValidate(email)
-
-  const confirmedPw = document.querySelector('#confirmpw')
-  va.passwordNotConfirmed(confirmedPw, psw)
-
   document.querySelector('#registerForm').addEventListener('submit', function (e) {
     e.preventDefault()
 
     const formData = new FormData(e.target)
     const userData = Object.fromEntries(formData)
 
-    if (va.isFormDataHasInvaild(userData)) {
+    if (Bootstrap5Validation().isFormDataHasInvaild(userData)) {
       alert('Please complete the register form')
     } else {
       addUserAndSendVerificationCode(JSON.stringify(userData))
     }
   })
-
-  Bootstrap5ShowPassword(document.querySelector('#showPsw'), '#password')
-  Bootstrap5ShowPassword(document.querySelector('#showConfirmPsw'), '#confirmpw')
 }
